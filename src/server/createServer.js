dfeb5861ba7216a0e14873ed4dc068c2637dc354
rubyrubenstahl <ruby@ruby-lighting.com>
2018-11-09 13:31:06 -0500

    Adds buffering & rate limiting of e:script packet delivery.

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -1,15 +1,17 @@
 const dgram = require("dgram");
-const {compileScript} = require('../lib')
+const { compileScript } = require('../lib')
 
 const defaultOpts = {
-  interface: "localhost",
+  hostAddress: "localhost",
   listenPort: 9001,
   sendAddress: "127.0.0.1",
   sendPort: 9000
 };
 
 function createServer(options = {}) {
-  const { interface, listenport, sendAddress, sendPort } = {
+  this.queue = [];
+
+  const { hostAddress, listenport, sendAddress, sendPort } = {
     ...defaultOpts,
     ...options
   };
@@ -17,17 +19,30 @@ function createServer(options = {}) {
   const socket = dgram.createSocket("udp4");
   socket.bind(9001);
 
-  this.runScript = function runScript(script){
-    const packetData = compileScript(script);
-    socket.send(packetData, sendPort, sendAddress, err => {
-          if (err) {
-            console.error('Error sending script to programmer: ' + err.message)
-          }
-        }
-    );
+  this.runScript = function runScript(script) {
+    this.queue.push(script);
   };
+
+  setInterval(() => {
+    if (this.queue.length > 0) {
+      const script = this.queue.shift();
+      this._sendScript(script);
+    }
+  }, 16);
+
+  this._sendScript = function (script) {
+    const packetData = compileScript(script);
+    console.log('Sending script: TS=' + new Date())
+
+    socket.send(packetData, sendPort, sendAddress, err => {
+      if (err) {
+        console.error('Error sending script to programmer: ' + err.message)
+      }
+    }
+    );
+  }
   return this;
 }
 
-module.exports =  createServer;
+module.exports = createServer;
 
