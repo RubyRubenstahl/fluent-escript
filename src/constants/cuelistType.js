ce7bf08d531e371e8cba5cd99019373b2f2183b6
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-11-12 15:06:34 -0500

    Adds more cuelist & cue constants.

---
+++ src/constants/cuelistType.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	normal: 0,
+	chaseForward: 1,
+	chaseRevers: 2,
+	chaseFlip: 3,
+	chaseRandom: 4
+}
---
+++ src/constants/playWhileRunningModes.js
@@ -0,0 +1,6 @@
+module.exports = {
+	nextCue: 0,
+	restartCuelist: 1,
+	ignore: 2,
+	stopCuelist: 3
+}
